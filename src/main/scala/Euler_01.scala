e20cbe5c92cc1b2fc600e4ee38863cb0c46aee4d
unknown <erikhansendk@hotmail.com>
2018-06-27 13:40:33 +0200

    better Euler01

--- src/main/scala/Euler_01.scala
+++ src/main/scala/Euler_01.scala
@@ -1,11 +1,10 @@
 object Euler_01 {
   def main(args: Array[String]): Unit = {
+
     // https://projecteuler.net/problem=1
 
-    val multOf3Or5 = (1 to 999).toList.filter(x => x % 3 == 0 ||  x % 5 == 0)
-    val res = multOf3Or5.sum
+    val res = (1 until 1000).filter(x => x % 3 == 0 ||  x % 5 == 0).sum
 
     println ( res )  // 233168
-
   }
 }
